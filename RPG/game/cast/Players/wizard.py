c25efca7994e158aabe4d8ae618dc4639fdf38df
dawsonpacker <91807575+dawsonpacker@users.noreply.github.com>
2022-03-30 16:48:52 -0600

    d

---
+++ RPG/game/cast/Players/wizard.py
@@ -0,0 +1,14 @@
+from game.cast.actor import Actor
+from game.common.skills import Skills
+class Archer(Actor):
+
+    def __init__(self, x, y):
+        super().__init__()
+        self._location = Location(x, y)
+        self._symbol = "?"
+        self._text_size = 50
+        self._color = (0,0,0)
+        self.skills = Skills(,,,,)
+
+    def get_skills():
+        return self.skills
