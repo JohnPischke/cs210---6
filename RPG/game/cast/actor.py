4358a7dfa90330bf1a577f4a753a933d21d1bb70
JohnPischke <100436081+JohnPischke@users.noreply.github.com>
2022-03-30 16:52:08 -0600

    Update actor.py

--- RPG/game/cast/actor.py
+++ RPG/game/cast/actor.py
@@ -1,4 +1,34 @@
 
-Class Actor:
+from game.common.location import Location
+class Actor:
 
+    def __init__(self):
+        self._location = Location(x, y)
+        self._symbol = "#"
+        self._text_size = 50
+        self._color = (0,0,0)
+    
+    def get_location (self):
+        return self._location
+
+    def get_symbol (self):
+        return self._symbol
+
+    def get_text_size (self):
+        return self._text_size
+
+    def get_color (self):
+        return self._color
+
+    def set_location (self, value):
+        self._location = value
+
+    def set_symbol (self, value):
+        self._symbol = value
+
+    def set_text_size (self, value):
+        self._text_size = value
+
+    def set_color (self, value):
+        self._color = value
     
