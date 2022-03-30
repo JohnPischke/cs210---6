5e895dcc12bc91fa1fb709f9b8e92dd292e3cac4
dawsonpacker <91807575+dawsonpacker@users.noreply.github.com>
2022-03-30 16:20:11 -0600

    d

---
+++ RPG/game/cast/Players/archer.py
@@ -0,0 +1,11 @@
+class archer: 
+    def size(self): 
+        self.size = 3
+        return self.size
+
+    def symbol(self): 
+        self.symbol = '-->'
+        return self.symbol
+
+    def color(self)
+        self.color = 
